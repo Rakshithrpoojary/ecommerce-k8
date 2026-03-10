ad6761c8dc746dc5489280482693828e21e7a187
Rakshith <poojaryrakshith986@gmail.com>
2026-03-10 18:28:05 +0530

    new

--- frontend/src/helpers/uploadImage.js
+++ frontend/src/helpers/uploadImage.js
@@ -1,9 +1,9 @@
 // const fs = require("fs");
 // const REACT_APP_CLOUD_NAME_CLOUDINARY = fs.readFileSync("/mnt/secrets/REACT_APP_CLOUD_NAME_CLOUDINARYS","utf8").trim();
 // console.log(REACT_APP_CLOUD_NAME_CLOUDINARY);
-const REACT_APP_CLOUD_NAME_CLOUDINARY = dnbnsimy4
+const REACT_APP_CLOUD_NAME_CLOUDINARY = "dnbnsimy4"
 const url = `https://api.cloudinary.com/v1_1/${REACT_APP_CLOUD_NAME_CLOUDINARY}/image/upload`
-
+console.log(url);
 const uploadImage  = async(image) => {
     const formData = new FormData()
     formData.append("file",image)
